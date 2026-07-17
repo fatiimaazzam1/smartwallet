ce5503cbdf6857e2517312021e4be7863586d3d0
fatiimaazzam1 <fatiimaazzam@gmail.com>
2026-07-18 01:29:28 +0300

    chore(backend): initialize Spring Boot project

---
+++ backend/src/main/java/com/smartwallet/backend/SmartwalletBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartwallet.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartwalletBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartwalletBackendApplication.class, args);
+	}
+
+}
