f61ef6af30298eab1784dc0c400b56189045b66b
fatiimaazzam <fatiimaazzam@gmail.com>
2026-07-31 19:23:55 +0300

    Revert "feat(mobile-auth): complete authentication and password recovery (#14)"
    This reverts commit b7de4bd708638d662e5ec3f22dbf49b60ac64c5a.

--- mobile/lib/features/auth/data/models/refresh_request_model.dart
+++
@@ -1,9 +0,0 @@
-final class RefreshRequestModel {
-  const RefreshRequestModel({required this.refreshToken});
-
-  final String refreshToken;
-
-  Map<String, dynamic> toJson() {
-    return <String, dynamic>{'refreshToken': refreshToken};
-  }
-}
